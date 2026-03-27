d0c9e554b749cf8fe9db0d91524e2484ca1d7828
ritikamadaan18 <rashishvi@gmail.com>
2026-03-27 14:51:12 -0500

    first commit

---
+++ app/modal.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+
+export default function ModalScreen() {
+  return (
+    <Link href="/" dismissTo style={styles.link}>
+      <Text>Go to home screen</Text>
+    </Link>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
