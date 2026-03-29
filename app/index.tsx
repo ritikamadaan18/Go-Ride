196085f0959641b03f882534ed88ac13166861ee
ritikamadaan18 <rashishvi@gmail.com>
2026-03-29 18:59:22 -0500

    Added sign in fucntionality using Clerk

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
+import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 import React from "react";
 
 const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
   return <Redirect href="/(auth)/welcome" />;
 };
 
